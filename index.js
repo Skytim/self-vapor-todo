c34878a42d29f5def207b17c163a1c9af40a54d2
Skytim <sky22357168@gmail.com>
2024-09-11 14:19:46 +0800

    fix: code change

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const title = document.createElement('h1');
 const input = document.createElement('input');
 
 let titleText = 'Hello World!';
-
-title.textContent = titleText;
-input.value = titleText;
-
-// 事件處理
 input.addEventListener('input', (e) => titleTextChange(e.target.value));
 
-// 狀態更新
 function titleTextChange(newTitleText) {
-    title.textContent = newTitleText;
-    input.value = newTitleText;
+    titleText = newTitleText;
+    render();  // 重新渲染畫面
 }
 
+function render() {
+    title.textContent = titleText;
+    input.value = titleText;
+}
+
+render();  // 初始化渲染
 container.append(title, input);
 document.querySelector('#app').appendChild(container);
